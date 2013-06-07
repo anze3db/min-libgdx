815d999fbf7d5f965e23e8b4cf90f9d4d1b7d7fa
Smotko <ensmotko@gmail.com>
2013-06-07 11:57:11 +0200

    not working, shader error

---
+++ min-libgdx/src/com/psywerx/min/MeshHelper.java
@@ -0,0 +1,84 @@
+package com.psywerx.min;
+
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Mesh;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.VertexAttribute;
+import com.badlogic.gdx.graphics.VertexAttributes.Usage;
+import com.badlogic.gdx.graphics.glutils.ShaderProgram;
+
+public class MeshHelper {
+  private Mesh mesh;
+  private ShaderProgram meshShader;
+  
+  public MeshHelper(){
+    createShader();
+  }
+  
+  public void createMesh(float[] vertices){
+    mesh = new Mesh(true, vertices.length, 0, 
+        new VertexAttribute(Usage.Position, 3, "a_position"), 
+        new VertexAttribute(Usage.Normal, 3, "a_normal"),
+        new VertexAttribute(Usage.ColorPacked, 4, "a_color"));
+    
+    mesh.setVertices(vertices);
+    
+
+  }
+  
+  public void drawMesh(OrthographicCamera camera){
+    if(mesh == null){
+      throw new IllegalStateException("Draw mesh called before mash was initialized");
+    }
+    meshShader.begin();
+    meshShader.setUniformMatrix("u_worldView", camera.combined);
+    meshShader.setUniform3fv("u_lightPos", new float[] {0,0,0}, 0, 3);
+    mesh.render(meshShader, GL20.GL_TRIANGLES);
+    meshShader.end();
+  }
+  
+  private void createShader(){
+    // this shader tells opengl where to put things
+    String vertexShader = "uniform mat4 u_worldView;     \n"
+                        + "uniform vec3 u_lightPos;      \n"
+
+                        + "attribute vec4 a_position;    \n"
+                        + "attribute vec3 a_normal;      \n"
+                        + "attribute vec4 a_color;       \n"
+                        
+                        + "varying vec4 v_color;         \n"
+                        
+                        + "void main()                   \n"
+                        + "{                             \n"
+                        + "   vec3 modelViewVertex = vec3(u_worldView * vec4(a_position, 0.0));          \n"
+                        + "   vec3 lightVector = normalize(u_lightPos - modelViewVertex);     \n"
+                        + "   vec3 modelViewNormal = vec3(u_worldView * vec4(a_normal, 0.0)); \n"
+                        + "   float diffuse = max(dot(modelViewNormal, lightVector), 0.1);    \n"
+                        + "   v_color = a_color * diffuse;                                    \n"
+                        + "   gl_Position = modelViewVertex;                                  \n"
+                        + "}                             \n";
+
+    // this one tells it what goes in between the points (i.e
+    // colour/texture)
+    String fragmentShader = "#ifdef GL_ES                \n"
+                          + "precision mediump float;    \n"
+                          + "#endif                      \n"
+                          + "varying vec4 v_color;       \n"
+                          + "void main()                 \n"
+                          + "{                           \n"
+                          + "  gl_FragColor = v_color;   \n"
+                          + "}";
+
+    // make an actual shader from our strings
+    meshShader = new ShaderProgram(vertexShader, fragmentShader);
+
+    // check there's no shader compile errors
+    if (!meshShader.isCompiled())
+      throw new IllegalStateException(meshShader.getLog());
+  }
+  
+  public void dispose(){
+    mesh.dispose();
+    meshShader.dispose();
+  }
+}
--- min-libgdx/src/com/psywerx/min/MinGame.java
+++ min-libgdx/src/com/psywerx/min/MinGame.java
@@ -2,19 +2,13 @@ package com.psywerx.min;
 
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL10;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.Texture.TextureFilter;
-import com.badlogic.gdx.graphics.g2d.Sprite;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
 
 public class MinGame implements ApplicationListener {
 	private OrthographicCamera camera;
-	private SpriteBatch batch;
-	private Texture texture;
-	private Sprite sprite;
+  private MeshHelper meshHelper;
 	
 	@Override
 	public void create() {		
@@ -22,34 +16,26 @@ public class MinGame implements ApplicationListener {
 		float h = Gdx.graphics.getHeight();
 		
 		camera = new OrthographicCamera(1, h/w);
-		batch = new SpriteBatch();
 		
-		texture = new Texture(Gdx.files.internal("data/libgdx.png"));
-		texture.setFilter(TextureFilter.Linear, TextureFilter.Linear);
-		
-		TextureRegion region = new TextureRegion(texture, 0, 0, 512, 275);
-		
-		sprite = new Sprite(region);
-		sprite.setSize(0.9f, 0.9f * sprite.getHeight() / sprite.getWidth());
-		sprite.setOrigin(sprite.getWidth()/2, sprite.getHeight()/2);
-		sprite.setPosition(-sprite.getWidth()/2, -sprite.getHeight()/2);
+		meshHelper = new MeshHelper();
+    meshHelper.createMesh(new float[] { 
+        -0.5f, -0.5f, 0,  1, 0, 0,  1f, 0, 0, 0,
+         0.5f, -0.5f, 0,  1, 0, 0,  1f, 0, 0, 0,  
+           0f,  0.5f, 0,  1, 0, 0,  1f, 0, 0, 0});
 	}
-
+	
 	@Override
 	public void dispose() {
-		batch.dispose();
-		texture.dispose();
+		meshHelper.dispose();
 	}
 
 	@Override
 	public void render() {		
 		Gdx.gl.glClearColor(1, 1, 1, 1);
-		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
-		
-		batch.setProjectionMatrix(camera.combined);
-		batch.begin();
-		sprite.draw(batch);
-		batch.end();
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		camera.rotate(-0.1f);
+		camera.update();
+		meshHelper.drawMesh(camera);
 	}
 
 	@Override
